72b14acb3f8ac48ad212edba281726535be6a5ae
Musa <malikmusa1323@gmail.com>
2024-06-10 02:04:50 -0700

    fix localization error

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,14 +1,17 @@
 import {notFound} from 'next/navigation';
-import {getRequestConfig} from 'next-intl/server';
+import {getRequestConfig, unstable_setRequestLocale} from 'next-intl/server';
 
 // Add vietnamese locale
 const locales = ['en', 'es', 'zh', 'so', 'vi', 'fl', 'ar', 'ur', 'pn'];
 
 export default getRequestConfig(async ({locale}) => {
     // Validate that the incoming `locale` parameter is valid
-    if (!locales.includes(locale as any)) notFound();
+    if (!locales.includes(locale as any)) throw new Error('Language not supported');
+
+    // Set the request locale for static rendering
+    await unstable_setRequestLocale(locale);
 
     return {
-        messages: (await import(`../locales/${locale}.json`)).default
+        messages: (await import(`../locales/${locale}.json`)).default,
     };
 });
